c0251bbc961e851184b5a7cc71c3de8036670355
kartik <kartik.dongre@galaxyweblinks.in>
2024-05-07 19:37:34 +0530

    Worked on the new changes in admin module

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -217,3 +217,20 @@ $( document ).ready(function() {
 
     });
 });
+
+// upload file code
+function readUrl(input) {
+  
+    if (input.files && input.files[0]) {
+      let reader = new FileReader();
+      reader.onload = (e) => {
+        let imgData = e.target.result;
+        let imgName = input.files[0].name;
+        input.setAttribute("data-title", imgName);
+        console.log(e.target.result);
+         $('#preview').html('<img src="' + e.target.result + '" />')
+      }
+      reader.readAsDataURL(input.files[0]);
+    }
+  
+  }
